4464938d577e272e8ff74354edc7efc2592c1e09
MaxKolosovskiy <cs.go.kot666@gmail.com>
2024-03-01 08:52:22 +0200

    Added lab1

---
+++ laba1.py
@@ -0,0 +1,20 @@
+def is_array_monotonic(array):
+    increasing = False
+    decreasing = False
+
+    if len(array) == 1:
+        return True
+
+    for i in range(1, len(array)):
+        if array[i] < array[i - 1]:
+
+            decreasing = True
+
+    for i in range(1, len(array)):
+        if array[i] > array[i - 1]:
+
+            increasing = True
+
+    if increasing and decreasing:
+        return False
+    return True
---
+++ test_laba1.py
@@ -0,0 +1,19 @@
+import unittest
+from laba1 import is_array_monotonic
+
+class Testlab1(unittest.TestCase):
+    def test_increasing(self):
+        self.assertTrue(is_array_monotonic([1, 4, 7, 7, 15]))
+
+    def test_decreasing(self):
+        self.assertTrue(is_array_monotonic([10, 8, 4, 1]))
+
+    def test_not_monotonic(self):
+        self.assertFalse(is_array_monotonic([1, 5, 2, 4, 15]))
+
+    def test_is_array_monotoniс(self):
+        self.assertTrue(is_array_monotonic([7]))
+
+    def test_none(self):
+        self.assertTrue(is_array_monotonic([]))
+
